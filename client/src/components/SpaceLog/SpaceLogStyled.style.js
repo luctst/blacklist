7dd14a26f72a48afb9f834862a20387c56d30ed9
Kévin Joya <kevin.joya@outlook.com>
2020-01-20 18:17:27 +0100

    Created spaceLog style

---
+++ client/src/components/SpaceLog/SpaceLogStyled.style.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const SpaceLogStyled = styled.div`
+  font-family: Roboto;
+  margin-top: 8%;
+    label {
+      font-family: Gelasio;
+      font-weight: bold;
+    }
+    input {
+      background: #f5f6f7;
+      padding: 9px 15px;
+      border-radius: 6px;
+      border: none;
+    }
+`
+
+export default SpaceLogStyled
