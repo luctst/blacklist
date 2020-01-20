6769125fac3b33622fb22cbacbd8377e63f18eb9
Kévin Joya <kevin.joya@outlook.com>
2020-01-20 18:16:35 +0100

    Added compos render

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
+import Title from './Header/Title';
+import SpaceLog from "../components/SpaceLog/SpaceLog";
+import {
+  Route,
+  BrowserRouter
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Title />
+        <div className="container" style={{display: "flex", justifyContent: "center"}}>
+          <Route exact path="/" component={SpaceLog} />
+          <Route exact path="/inscription" component={SpaceLog} />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
