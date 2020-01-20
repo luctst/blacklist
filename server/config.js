abe5c7e51d0d3039c920549744e93914eeb554b1
Lucas Tostée <lucas.tostee@gmail.com>
2020-01-20 15:26:35 +0100

    Fix eslint

--- server/config.js
+++ server/config.js
@@ -1,15 +1,15 @@
-const {config} = require("dotenv");
+const { config } = require("dotenv");
 
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 const envfound = config();
 
 if (!envfound) {
-    throw new Error("⚠️  Couldn't find .env file  ⚠️");
+  throw new Error("⚠️  Couldn't find .env file  ⚠️");
 }
 
 if (process.env.NODE_ENV === "development") {
-    process.env.url = "localhost";
+  process.env.url = "localhost";
 } else {
-    process.env.url = "https://apiurl.com/";
+  process.env.url = "https://apiurl.com/";
 }
