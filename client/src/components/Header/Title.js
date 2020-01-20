fe68a2d6b4da22f2b4fe6bcf3d87a6d1047498d7
Kévin Joya <kevin.joya@outlook.com>
2020-01-20 18:16:43 +0100

    Created Title compo

---
+++ client/src/components/Header/Title.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TitleStyled from "./TitleStyled.style";
+
+function Title() {
+  return(
+    <header>
+      <TitleStyled className="header--title mt-5">
+        <h1>BlackList</h1>
+      </TitleStyled>
+    </header>
+  )
+}
+
+export default Title
