a15d94c2f07ead817ab55fb6bdb35eb707970555
Lucas Tostée <lucas.tostee@gmail.com>
2020-01-20 15:27:10 +0100

    fix eslint

--- server/db/index.js
+++ server/db/index.js
@@ -3,11 +3,11 @@ const { MongoClient } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_KEY}@web-timeline-dvcrh.mongodb.net/test?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
 module.exports = async () => {
-    const connexion = await client.connect();
-    return connexion.db("web-timeline");
+  const connexion = await client.connect();
+  return connexion.db("web-timeline");
 };
--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-/*estlint-disable no-console*/
+/* estlint-disable no-console */
 require("./config");
-const {getPortPromise} = require("portfinder");
-const {promises} = require("fs");
-const {basename} = require("path");
-const {createServer} = require("http");
+const { getPortPromise } = require("portfinder");
+const { promises } = require("fs");
+const { basename } = require("path");
+const { createServer } = require("http");
 const api = require("./api/index");
 
 /**
@@ -12,10 +12,10 @@ const api = require("./api/index");
  * @returns An integer who correspont to an available port.
  */
 async function getAvailablePort(reqPort) {
-    const baseport = parseInt(reqPort, 10);
-    const port = await getPortPromise({ port: baseport });
+  const baseport = parseInt(reqPort, 10);
+  const port = await getPortPromise({ port: baseport });
 
-    return port;
+  return port;
 }
 
 /**
@@ -23,26 +23,28 @@ async function getAvailablePort(reqPort) {
  * @param {String} path The directory to parse
  */
 async function getRoutesApi(directoryPath) {
-    return (await promises.readdir(directoryPath)).map(route => {
-        if (basename(route, ".js") === "default") return "/";
-        return `/${basename(route, ".js")}`;
-    });
+  return (await promises.readdir(directoryPath)).map(route => {
+    if (basename(route, ".js") === "default") return "/";
+    return `/${basename(route, ".js")}`;
+  });
 }
 
 (async () => {
-    process.env.PORT = await getAvailablePort(8080);
-    const routes = await getRoutesApi(`${process.cwd()}/server/api/routes`);
+  process.env.PORT = await getAvailablePort(8080);
+  const routes = await getRoutesApi(`${process.cwd()}/server/api/routes`);
 
-    createServer((req, res) => api(req, res, routes))
-    .listen({
-        port: process.env.PORT,
-        host: process.env.url
-    }, () => console.log(`Server listen here http://${process.env.url}:${process.env.PORT}`));
+  createServer((req, res) => api(req, res, routes)).listen(
+    {
+      port: process.env.PORT,
+      host: process.env.url
+    },
+    () => console.log(`Server listen here http://${process.env.url}:${process.env.PORT}`)
+  );
 })();
 
 ["SIGINT", "SIGTERM"].forEach(signal => {
-    process.on(signal, () => {
-        console.log(`Server closed, see you :)`);
-        process.exit();
-    });
+  process.on(signal, () => {
+    console.log(`Server closed, see you :)`);
+    process.exit();
+  });
 });
