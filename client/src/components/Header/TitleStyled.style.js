8f8be847c43affdcf71464788d3aaf315fa45da2
Kévin Joya <kevin.joya@outlook.com>
2020-01-20 18:16:53 +0100

    Created title style

---
+++ client/src/components/Header/TitleStyled.style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const TitleStyled =  styled.div`
+  display: flex;
+  justify-content: center;
+    h1 {
+      font-family: Gelasio;
+  }
+`
+
+export default TitleStyled
