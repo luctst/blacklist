3d0aaffc87ae24591fedd35a825a428c0abf9861
Kévin Joya <kevin.joya@outlook.com>
2020-01-22 12:08:30 +0100

    Added ul tag

--- client/src/components/BlackList/BlackList.js
+++ client/src/components/BlackList/BlackList.js
@@ -5,17 +5,18 @@ function BlackList() {
   return(
     <BlackListStyled className="container my--blacklist mt-3">
       <div className="my--blacklist--form--name form-group">
-        {/* <label htmlFor="inputName">The secret identity:</label> */}
         <input type="text" id="inputName" className="form-control" placeholder="The secret identity" />
         {/* <input type="file" id="avatar" name="avatar" accept="image/png, image/jpeg"></input> */}
         <button className="btn btn-primary">Blacklisted</button>
       </div>
-      <li>Cloé</li>
-      <li>Amélie</li>
-      <li>Jennifer</li>
-      <li>Ophélie</li>
-      <li>Fiona</li>
-      <li>Dyson</li>
+      <ul>
+        <li>Cloé</li>
+        <li>Amélie</li>
+        <li>Jennifer</li>
+        <li>Ophélie</li>
+        <li>Fiona</li>
+        <li>Dyson</li>
+      </ul>
     </BlackListStyled>
   )
 }
