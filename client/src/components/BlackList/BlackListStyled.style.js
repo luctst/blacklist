5ce3068d72e7fcec00cf932e1fdfa786cab6bf58
Kévin Joya <kevin.joya@outlook.com>
2020-01-22 14:21:26 +0100

    Cleaned code and delete style

--- client/src/components/BlackList/BlackListStyled.style.js
+++ client/src/components/BlackList/BlackListStyled.style.js
@@ -2,30 +2,13 @@ import styled from "styled-components";
 
 const BlackListStyled = styled.section`
   font-family: Roboto;
-    label {
-      font-family: Gelasio;
-      font-weight: bold;
-    }
-    input {
+    label, input {
       width: 20%;
       padding: 9px 15px;
       border: none;
         :focus {
           box-shadow: none;
           background-color: #f5f6f7;
-        }
-    }
-    #avatar {
-      width: 30%;
-    }
-    button, button:hover {
-      background: #333;
-      border: none;
-      margin: 20px 10px;
-        :focus {
-          box-shadow: none;
-          background-color: #333;
-          border: none;
         }
     }
 `
