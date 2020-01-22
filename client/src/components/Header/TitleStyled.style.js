312d75898c771783a558cf3b4eaac909e4e175ec
Kévin Joya <kevin.joya@outlook.com>
2020-01-22 12:09:06 +0100

    Changed style

--- client/src/components/Header/TitleStyled.style.js
+++ client/src/components/Header/TitleStyled.style.js
@@ -1,11 +1,8 @@
 import styled from "styled-components";
 
-const TitleStyled =  styled.div`
-  display: flex;
-  justify-content: center;
-    h1 {
-      font-family: Gelasio;
-  }
+const TitleStyled =  styled.h1`
+  text-align: center;
+  font-family: Gelasio;
 `
 
 export default TitleStyled
