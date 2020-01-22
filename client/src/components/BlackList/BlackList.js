1096a47837b580dbb6222239c1b3fbaa1d24035c
Kévin Joya <kevin.joya@outlook.com>
2020-01-22 14:21:10 +0100

    Added input files and fn to add when key press

--- client/src/components/BlackList/BlackList.js
+++ client/src/components/BlackList/BlackList.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, {useState, useRef} from "react";
 import BlackListStyled from "./BlackListStyled.style";
 
 function BlackList() {
+  const inputNameValue = useRef(null);
+  const [data, setData] = useState({
+    name: "",
+    image: null
+  })
+
+  function addName(e) {
+    const newState = {...data}
+    if (e.keyCode === 13) {
+      newState.name = inputNameValue.current.value;
+      setData(newState)
+    }
+  }
+
   return(
     <BlackListStyled className="container my--blacklist mt-3">
       <div className="my--blacklist--form--name form-group">
-        <input type="text" id="inputName" className="form-control" placeholder="The secret identity" />
-        {/* <input type="file" id="avatar" name="avatar" accept="image/png, image/jpeg"></input> */}
-        <button className="btn btn-primary">Blacklisted</button>
+        <input type="text" id="inputName" className="form-control" placeholder="The secret identity" ref={inputNameValue} onKeyDown={addName} />
+        <label htmlFor="inputImg" className="form-control ml-3 mb-5">The secret image</label>
+        <input type="file" id="inputImg" accept="image/png, image/jpeg" style={{display: "none"}}></input>
       </div>
       <ul>
         <li>Cloé</li>
