dda31de33ad6e6a5c5de83b279fd3a19601d7f5a
Kévin Joya <kevin.joya@outlook.com>
2020-01-22 19:47:36 +0100

    Changed all style

--- client/src/components/BlackList/BlackListStyled.style.js
+++ client/src/components/BlackList/BlackListStyled.style.js
@@ -2,18 +2,45 @@ import styled from "styled-components";
 
 const BlackListStyled = styled.section`
   font-family: Roboto;
-    input {
-      width: 20%;
-      padding: 9px 15px;
-      border: none;
-        :hover {
-          background-color: #f5f6f7;
+  .parent--div {
+    position: relative;
+    display: flex;
+    align-items: center;
+    flex: auto;
+
+    .parent--dot {
+      margin-right: 4px;
+      width: 24px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-grow: 0;
+      flex-shrink: 0;
+      min-height: calc(1.5em + 3px + 3px);
+      padding-right: 2px;
+
+        span {
+          width: 6px;
+          height: 6px;
+          border-radius: 6px;
+          background: currentcolor;
+          margin-top: 0.1em;
         }
-        :focus {
-          box-shadow: none;
-          background-color: #f5f6f7;
-        }
+      }
+    .content--name:focus {
+      outline: none;
     }
+
+    .parent--div--close {
+      position: absolute;
+      left: -12px;
+      display: none;
+    }
+
+    :hover > .parent--div--close {
+      display: block;
+    }
+  }
 `
 
 export default BlackListStyled
