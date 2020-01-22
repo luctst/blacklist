38e1f2b6faee14a4fe9fb37f8452a59cb16b15c4
Kévin Joya <kevin.joya@outlook.com>
2020-01-22 15:24:19 +0100

    Changed style

--- client/src/components/BlackList/BlackListStyled.style.js
+++ client/src/components/BlackList/BlackListStyled.style.js
@@ -2,10 +2,13 @@ import styled from "styled-components";
 
 const BlackListStyled = styled.section`
   font-family: Roboto;
-    label, input {
+    input {
       width: 20%;
       padding: 9px 15px;
       border: none;
+        :hover {
+          background-color: #f5f6f7;
+        }
         :focus {
           box-shadow: none;
           background-color: #f5f6f7;
