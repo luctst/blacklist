83d9290dd84eea8f3d3b959d17d7b94765892dd4
Kévin Joya <kevin.joya@outlook.com>
2020-01-22 14:52:53 +0100

    Changed tag name

--- client/src/components/Header/TitleStyled.style.js
+++ client/src/components/Header/TitleStyled.style.js
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 
-const TitleStyled =  styled.h1`
-  text-align: center;
-  margin-top: 3%;
-  font-family: Gelasio;
+const TitleStyled =  styled.header`
+  h1 {
+    text-align: center;
+    margin-top: 3%;
+    font-family: Gelasio;
+  }
 `
 
 export default TitleStyled
