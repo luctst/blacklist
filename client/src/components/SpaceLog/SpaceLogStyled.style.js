b8fda5766705895320417f7dc92ef7b5d28692ec
Kévin Joya <kevin.joya@outlook.com>
2020-01-22 12:09:56 +0100

    Changed div to form and added style

--- client/src/components/SpaceLog/SpaceLogStyled.style.js
+++ client/src/components/SpaceLog/SpaceLogStyled.style.js
@@ -1,14 +1,17 @@
 import styled from "styled-components";
 
-const SpaceLogStyled = styled.div`
+const SpaceLogStyled = styled.form`
   font-family: Roboto;
   margin-top: 8%;
+  margin-left: 40%;
+  margin-right: 40%;
     label {
       font-family: Gelasio;
       font-weight: bold;
     }
     input {
       background: #f5f6f7;
+      width: 30%;
       padding: 9px 15px;
       border-radius: 6px;
       border: none;
