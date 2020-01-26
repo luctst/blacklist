42abe2f2b32ea9cda55aaccc89480a70dffa716f
Lucas Tostée <lucas.tostee@gmail.com>
2020-01-27 00:46:44 +0100

    Create users routes api logic

---
+++ server/api/routes/users.js
@@ -0,0 +1,44 @@
+const { POST, PUT, DELETE } = require("../../db/services/users");
+
+module.exports = request => {
+  if (!["POST", "PUT", "DELETE"].includes(request.method)) {
+    return Promise.resolve({
+      error: true,
+      data: {
+        status: "Not found",
+        message: "This route can only be access with POST, PUT, DELETE method."
+      }
+    });
+  }
+
+  if (!request.header.hasOwnProperty("authorization")) {
+    return Promise.resolve({
+      error: true,
+      data: {
+        status: "Not found",
+        message: "You must add 'Authorization: Bearer <token>' header."
+      }
+    });
+  }
+
+  if (request.body === undefined) {
+    return Promise.resolve({
+      error: true,
+      data: {
+        status: "Not found",
+        message: "You must pass data."
+      }
+    });
+  }
+
+  switch (request.method) {
+    case "POST":
+      return POST().then(res => res);
+    case "PUT":
+      return PUT();
+    case "DELETE":
+      return DELETE();
+    default:
+      break;
+  }
+};
