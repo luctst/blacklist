837cb14476896c9e32fac383243492af3ca2813d
Kévin Joya <kevin.joya@outlook.com>
2020-01-27 15:26:19 +0100

    Changed icon to delete and added function to do it

--- client/src/components/BlackList/BlackList.js
+++ client/src/components/BlackList/BlackList.js
@@ -13,7 +13,13 @@ function BlackList() {
       document.activeElement.blur();
       setData(newState);
     }
+  }
 
+  function deleteName(index) {
+    const newState = [...data];
+
+    newState.splice(index, 1);
+    setData(newState);
   }
 
   return (
@@ -28,7 +34,7 @@ function BlackList() {
               <div>
                 <div contentEditable="true" className="content--name" onKeyDown={(e) => addInput(e, index)}>{name}</div>
               </div>
-              <div className="parent--div--close">x</div>
+              <div className="parent--div--close" onClick={() => deleteName(index)}><i class="fas fa-times"></i></div>
             </div>
           )
         })
