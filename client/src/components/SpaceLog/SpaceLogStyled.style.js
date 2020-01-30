b06f82896ee914c6aebbfa0df9eef856bd0761ee
Lucas Tostée <lucas.tostee@gmail.com>
2020-01-30 15:55:39 +0100

    Update style

--- client/src/components/SpaceLog/SpaceLogStyled.style.js
+++ client/src/components/SpaceLog/SpaceLogStyled.style.js
@@ -7,26 +7,26 @@ const SpaceLogStyled = styled.form`
   flex-direction: column;
   align-items: center;
 
-  .form--connection--mail, .form--connection--password {
+  .form--connection--mail {
     display: flex;
     flex-direction: column;
-    margin-bottom: 5%;
+    margin-bottom: 3%;
   }
   .form--connection--button {
     input {
       margin-bottom: 10px;
     }
   }
-    label {
-      font-family: Gelasio;
-      font-weight: bold;
-    }
-    input {
-      background: #f5f6f7;
-      padding: 9px 15px;
-      border-radius: 6px;
-      border: none;
-    }
+  label {
+    font-family: Gelasio;
+    font-weight: bold;
+  }
+  input {
+    background: #f5f6f7;
+    padding: 9px 15px;
+    border-radius: 6px;
+    border: none;
+  }
 `
 
 export default SpaceLogStyled
