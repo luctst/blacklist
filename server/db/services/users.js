c0ff07c7a328dcdf83b2d08289b5989f0be27d3d
Lucas Tostée <lucas.tostee@gmail.com>
2020-01-30 15:52:57 +0100

    Create post fn for create user

--- server/db/services/users.js
+++ server/db/services/users.js
@@ -2,12 +2,64 @@ const db = require("../index");
 
 /**
  * Create a new user
+ * @param {Object} body Data send by the request
  */
-async function POST() {
-  console.log(await db());
+async function POST(body) {
+  const b = Object.keys(body);
+
+  if (b.length !== 1) {
+    return {
+      error: true,
+      code: 400,
+      data: {
+        status: 400,
+        message: "The body should only contains a pseudo property."
+      }
+    };
+  }
+
+  if (b[0] !== "pseudo") {
+    return {
+      error: true,
+      code: 400,
+      data: {
+        status: 400,
+        message: "Request body should have a pseudo property."
+      }
+    };
+  }
+
+  if (typeof body.pseudo !== "string") {
+    return {
+      error: true,
+      code: 400,
+      data: {
+        status: 400,
+        message: "Pseudo property should be a string."
+      }
+    };
+  }
+
+  const users = (await db()).collection("users");
+  const r = await users.findOne({ ...body });
+
+  if (r === null) {
+    await users.insertOne({ pseudo: body.pseudo });
+
+    return {
+      data: {
+        status: 200,
+        message: "User created"
+      }
+    }
+  }
+
   return {
+    error: true,
+    code: 409,
     data: {
-      message: "OK"
+      status: 409,
+      message: "User already exist."
     }
   };
 }
