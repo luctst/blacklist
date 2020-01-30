83150e0089e13eba61e787afd8c0dce0cdd9dd42
Lucas Tostée <lucas.tostee@gmail.com>
2020-01-30 15:56:28 +0100

    Create logic for create user in front

--- client/src/components/SpaceLog/SpaceLog.js
+++ client/src/components/SpaceLog/SpaceLog.js
@@ -3,17 +3,50 @@ import SpaceLogStyled from "./SpaceLogStyled.style";
 import { Link } from "react-router-dom";
 
 function SpaceLog(props) {
+  const input = React.useRef(null);
+  const [state, setState] = React.useState({
+    url: process.env.NODE_ENV === "development" ? process.env.REACT_APP_APIURLDEV : "",
+    serverMessage: ""
+  });
+
+  const createUser = e => {
+    e.preventDefault();
+    const newState = {...state};
+
+    if (input.current.value.length === 0) {
+      newState.serverMessage = "You must enter a pseudo.";
+
+      return setState(newState);
+    }
+
+    fetch(`${state.url}/users`, {
+      method: "post",
+      body: JSON.stringify({pseudo: input.current.value})
+    })
+    .then(res => res.json())
+    .then(data => {
+      const newState = {...state};
+
+      if (data.status !== 200) {
+        newState.serverMessage = data.message;
+
+        return setState(newState);
+      }
+
+      newState.serverMessage = data.message;
+      return setState(newState);
+    })
+  }
 
   return (
     <React.Fragment>
-      <SpaceLogStyled className="container form--connection">
+      <SpaceLogStyled className="container form--connection" onSubmit={createUser}>
         <div className="form--connection--mail">
-          <label htmlFor="mail">Mail:</label>
-          <input type="email" id="mail" placeholder="name@example.com" required />
-        </div>
-        <div className="form--connection--password">
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" required />
+          <label htmlFor="pseudo">Pseudo:</label>
+          <input type="text" id="pseudo" placeholder="heuss-l'enfoire" ref={input}/>
+          {
+            state.serverMessage.length !== 0 && <small className="text-danger" style={{fontSize: "70%", marginTop: "3%"}}>{state.serverMessage}</small>
+          }
         </div>
         <div className="form--connection--button">
           <input type="submit" value="Submit"></input>
