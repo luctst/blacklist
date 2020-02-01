534a168ce95184ed87a147d3fda32ef7d76c73d6
Lucas Tostée <lucas.tostee@gmail.com>
2020-02-01 01:18:28 +0100

    Update fn for simplicity

--- server/utils/response-header.js
+++ server/utils/response-header.js
@@ -1,10 +1,9 @@
 /**
  * Create the response header
  * @param {Object} reponse The reponse object return on http request.
- * @param {Boolean} error If true reponse should return an 404 header.
- * @param {Number} code The status code to send in the response.
+ * @param {Object} data An object with property to add in the header response
  */
-module.exports = (reponse, error = false, code) => {
+module.exports = (reponse, data) => {
   const defaultHeader = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Expose-Headers":
@@ -13,6 +12,7 @@ module.exports = (reponse, error = false, code) => {
     connection: "close",
     "Content-Security-Policy": "default-src 'none'",
     Server: `${process.env.url}.com`,
+    Status: data.code,
     "X-XSS-Protection": "1;mode=block",
     "Referrer-Policy": "origin-when-cross-origin, strict-origin-when-cross-origin",
     "X-Content-Type-Options": "nosniff",
@@ -20,20 +20,25 @@ module.exports = (reponse, error = false, code) => {
     "X-RateLimit-Limit": 60
   };
 
-  if (error) {
-    return reponse.writeHead(code, {
+  if (data.code >= 400 && data.code <= 600) {
+    if (data.code === 405) {
+      return reponse.writeHead(data.code, {
+        ...defaultHeader,
+        ...data.serverHeader
+      })
+    }
+
+    return reponse.writeHead(data.code, {
       ...defaultHeader,
       "Transfer-Encoding": "chunked",
-      Status: code
     });
   }
 
   // TODO: Fill the empty data.
-  return reponse.writeHead(200, {
+  return reponse.writeHead(data.code, {
     ...defaultHeader,
     "cache-control": "public, max-age=60, s-maxage=60",
     Etag: "",
-    Status: "200",
     Vary: "accept",
     Vary: "accept-encoding" // eslint-disable-line no-dupe-keys
   });
