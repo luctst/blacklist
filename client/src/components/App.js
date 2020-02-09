493d87f1d5160b866f51317e33548b403a943487
Lucas Tostée <lucas.tostee@gmail.com>
2020-02-09 02:42:22 +0100

    Handle redirection, use switch to stop when routes is meet

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,16 +3,25 @@ import Header from './Header';
 import SpaceLog from "./SpaceLog";
 import {
   Route,
-  BrowserRouter
+  BrowserRouter,
+  Redirect,
+  Switch
 } from "react-router-dom";
 import BlackList from './BlackList';
+import NotFound from "./NotFound";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Route exact path={["/", "/inscription"]} component={SpaceLog} />
-      <Route exact path="/blacklist" component={BlackList} />
+      <Switch>
+        <Route exact path={["/", "/inscription"]} component={SpaceLog} />
+        <Route exact path="/bl/:pseudo" render={() => {
+          if (sessionStorage.getItem("userId")) return <BlackList />
+          return <Redirect to="/"/>
+        }} />
+        <Route path="*" component={NotFound}/>
+      </Switch>
     </BrowserRouter>
   );
 }
