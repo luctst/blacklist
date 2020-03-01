eea73696ffdca9d6fec169e810d649b3c23b2760
Lucas Tostée <lucas.tostee@gmail.com>
2020-03-01 19:29:23 +0100

    Add style files for components

---
+++ client/src/styles/Header.style.js
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+
+export default styled.header`
+  h1 {
+    text-align: center;
+    margin: 3% 0;
+    font-family: Gelasio;
+  }
+  @media screen and (max-width: 500px) {
+    h1 {
+      margin-bottom: 10%;
+    }
+  }
+  @media screen and (min-width: 768px) and (max-width: 1024px) {
+    h1 {
+      margin-bottom: 6%;
+      font-size: 3em;
+    }
+  }
+`
