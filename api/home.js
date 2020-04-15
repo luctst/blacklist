ebd120afdceea73c591935eb7fa2f3e1dbcca1fd
luctst <lucas.tostee@gmail.com>
2020-04-15 16:24:25 +0200

    =Switch new structure, use now lambda fn

---
+++ api/home.js
@@ -0,0 +1,3 @@
+module.exports = function Home(req, res) {
+  res.json(JSON.stringify({ t: true }));
+};
--- server/db/index.js
+++
@@ -1,6 +0,0 @@
-const { MongoClient } = require("mongodb");
-
-module.exports = new MongoClient(process.env.DB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
