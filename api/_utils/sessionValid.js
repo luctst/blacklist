bfa6a7de5a1a70e073ab36b389ff2d3e7c09dd1d
luctst <lucas.tostee@gmail.com>
2020-04-28 11:56:15 +0200

    =Fix prettier errors

--- api/_utils/sessionValid.js
+++ api/_utils/sessionValid.js
@@ -1,5 +1,5 @@
-const {parse} = require("cookie");
-const {verify} = require("jsonwebtoken");
+const { parse } = require("cookie");
+const { verify } = require("jsonwebtoken");
 const mongo = require("../db/index");
 
 /**
@@ -8,39 +8,39 @@ const mongo = require("../db/index");
  * @param {String} JWT - A JWT.
  * @param {Object} [options = {}] - An object who modify the function behavior.
  */
-module.exports = function (cookie, JWT, options = {}) {
-    if (!cookie || typeof cookie !== "string") {
-        return {
-            error: true,
-            why: "Cookie is missing or not a string"
-        };
-    }
+module.exports = async function(cookie, JWT, options = {}) {
+  if (!cookie || typeof cookie !== "string") {
+    return {
+      error: true,
+      why: "Cookie is missing or not a string"
+    };
+  }
 
-    const cookieObject = parse(cookie);
+  const cookieObject = parse(cookie);
 
-    if (!cookieObject.sid || typeof cookieObject.sid !== "string") {
-        return {
-            error: true,
-            why: "Cookie sid property is not defined or not a string"
-        };
-    }
+  if (!cookieObject.sid || typeof cookieObject.sid !== "string") {
+    return {
+      error: true,
+      why: "Cookie sid property is not defined or not a string"
+    };
+  }
 
-    if (!JWT || typeof JWT !== "string") {
-        return {
-            error: true,
-            why: "JWT arguments is maybe not passed or not a string."
-        }
-    }
+  if (!JWT || typeof JWT !== "string") {
+    return {
+      error: true,
+      why: "JWT arguments is maybe not passed or not a string."
+    };
+  }
 
-    if (options && typeof options !== "object") {
-        return {
-            error: true,
-            why: "Options object must be an objet"
-        }
-    }
+  if (options && typeof options !== "object") {
+    return {
+      error: true,
+      why: "Options object must be an objet"
+    };
+  }
 
-    const opts = {
-        ...options
-    }
-    const mongoclient = await mongo();
+  const opts = {
+    ...options
+  };
+  const mongoclient = await mongo();
 };
